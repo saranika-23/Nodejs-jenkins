2ae14bcb5fd392026c3b9f6b8db51e2491569fb7
saranika-23 <saranikasaravanan04@gmail.com>
2026-05-13 22:59:08 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node.js DevOps Projectsssss 🚀");
+    res.send("Hello from Node.js DevOps Projectssssss 🚀");
 });
 
 app.get('/health', (req, res) => {
